4ec9c7f7074dece5c8e5070da7d60029e0be4e0c
Abdullahi Hussein <abdullahi.hussein@mabillagroup.com>
2024-11-06 01:53:19 +0100

    feat: install react-native datetimepicker

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -2,12 +2,7 @@ import Svg, { ClipPath, Defs, G, Path, SvgProps } from "react-native-svg";
 
 export const ArrowUpDownIcon = (props: SvgProps) => {
   return (
-    <Svg
-      width={24}
-      height={24}
-      fill="none"
-      {...props}
-    >
+    <Svg width={24} height={24} fill="none" {...props}>
       <G clipPath="url(#a)">
         <Path
           fill="#000"
@@ -104,6 +99,33 @@ export const CheckBoxDefaultIcon = (props: SvgProps) => {
           <Path fill="#fff" d="M0 0h24v24H0z" />
         </ClipPath>
       </Defs>
+    </Svg>
+  );
+};
+
+export const CalendarIcon = (props: SvgProps) => {
+  return (
+    <Svg
+      width={24}
+      height={24}
+      fill="none"
+      {...props}
+    >
+      <Path
+        stroke="#292D32"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={1.5}
+        d="M8 2v3M16 2v3M3.5 9.09h17M21 8.5V17c0 3-1.5 5-5 5H8c-3.5 0-5-2-5-5V8.5c0-3 1.5-5 5-5h8c3.5 0 5 2 5 5Z"
+      />
+      <Path
+        stroke="#292D32"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M11.995 13.7h.009M8.295 13.7h.01M8.295 16.7h.01"
+      />
     </Svg>
   );
 };
